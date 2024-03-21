cfb92a173f9e6258cb146566e0ba0798ed054eb5
YardenNezah <74673812+YardenNezah@users.noreply.github.com>
2024-03-21 11:02:18 +0200

    warnings

--- src/components/layout/UrlLink.jsx
+++ src/components/layout/UrlLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UrlLink = (props) => {
     return (
-        <a href={props.location} target="_blank" className={`cta-button ${props.className}`}>
+        <a href={props.location} target="_blank" rel="noreferrer" className={`cta-button ${props.className}`}>
             {props.title}
         </a>
     )
