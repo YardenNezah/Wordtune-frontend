b0420c106cab38ef999271dca5ca64570d12a825
YardenNezah <74673812+YardenNezah@users.noreply.github.com>
2024-03-22 11:00:45 +0200

    data container- error+loading

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -3,7 +3,7 @@ import loader from '../../images/loader.svg';
 
 const Loader = () => {
   return (
-    <img src={loader} alt="loading..."></img>
+    <div className='data-container'><img src={loader} alt="loading..."></img></div>
   )
 }
 
